34da734235c4bfef89f07c34ae1885105327631e
nutcas3 <maurice.nyanja.e@gmail.com>
2025-11-01 01:30:16 +0300

    refactor: update OpenZeppelin contract imports and inheritance
    - Changed PausableUpgradeable import path from security/ to utils/ directory
    - Replaced ReentrancyGuardUpgradeable with non-upgradeable ReentrancyGuard contract
    - Updated inheritance chain to use standard ReentrancyGuard instead of upgradeable version

--- contracts/src/strategies/BaseStrategy.sol
+++ contracts/src/strategies/BaseStrategy.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/IAegisStrategy.sol";
@@ -17,7 +17,7 @@ abstract contract BaseStrategy is
     IAegisStrategy,
     AccessControlUpgradeable,
     PausableUpgradeable,
-    ReentrancyGuardUpgradeable
+    ReentrancyGuard
 {
     using SafeERC20 for IERC20;
 
